7cc94acee19340661450e72d576e78ec199aefd0
Martin Landa <landa.martin@gmail.com>
2015-06-03 16:18:04 +0200

    fix okresy.py (use set() instead of list()

--- pygrass/okresy.py
+++ pygrass/okresy.py
@@ -6,11 +6,12 @@ okresy = VectorTopo('okresy_polygon', mapset='ruian')
 okresy.open('r')
 
 for o in okresy.viter('areas'):
-    pocet = 0
+    sousede = set()
     for b in o.boundaries():
         for n in b.get_left_right():
             if n != -1 and n != o.id:
-                pocet += 1
-    print (u'{:20}: {}'.format(o.attrs['nazev'], pocet))
+                sousede.add(n)
+    
+    print (u'{:20}: {}'.format(o.attrs['nazev'], len(sousede)))
 
 okresy.close()
