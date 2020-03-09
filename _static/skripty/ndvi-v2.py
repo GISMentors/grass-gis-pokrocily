e30c036b6ad9441b09e4e3ef170855565f4d6bb4
Martin Landa <landa.martin@gmail.com>
2020-03-09 11:24:09 +0100

    ndvi-v2 WIP

--- _static/skripty/ndvi-v2.py
+++ _static/skripty/ndvi-v2.py
@@ -1,17 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 #%module
-#% description: Creates reclassified NDVI.
+#% description: Creates reclassified NDVI based on given AOI.
 #%end
 #%option G_OPT_M_MAPSET
 #% required: yes
 #% answer: landsat
 #%end
-#%option
-#% key: output_postfix
-#% type: string
-#% description: Postfix for output maps
-#% answer: ndvi
+#%option G_OPT_V_MAP
+#% key: aoi
+#% answer: obce@ruian_praha
 #%end
 #%option G_OPT_F_INPUT
 #% key: classes
@@ -21,83 +19,82 @@
 import sys
 from subprocess import PIPE
 
-import grass.script as grass
+from grass.script import parser
 
 from grass.pygrass.modules import Module
 from grass.pygrass.gis import Mapset
+#from grass.pygrass.messages import Messenger
 
 def main():
     mapset = options['mapset']
-    ndvi = options['output_postfix']
-    
-    # pridat mapset do vyhledavaci cesty
-    Module('g.mapsets', mapset=mapset, operation='add', quiet=True)
+    aoi = options['aoi']
+    if '@' in aoi:
+        aoi_name = aoi.split('@')[0]
+    else:
+        aoi_name = aoi
     
     try:
         vis = Mapset(mapset).glist('raster', pattern='*B4')[0]
         nir = Mapset(mapset).glist('raster', pattern='*B5')[0]
     except IndexError:
         grass.fatal("Nelze najit vstupni kanaly")
-    r_ndvi= "r_{}".format(ndvi)
+    ndvi = "ndvi_{}".format(aoi_name)
+    r_ndvi= "r_ndvi_{}".format(aoi_name)
+
+    msgr = Messenger()
     
-    # nastavit vypocetni region
-    Module('g.region', raster=vis)
+    # 0. nastavit vypocetni region
+    Module('g.region', align=vis, vector=aoi)
     
-    # vypocet NDIV
-    grass.message("VIS: {0} ; NIR: {1}".format(vis, nir))
+    # 1. vypocet NDVI
+    msgr.message("VIS: {0} ; NIR: {1}".format(vis, nir))
     Module('r.mapcalc',
            expression="{o} = float({n} - {v}) / ({n} + {v})".format(o=ndvi, v=vis, n=nir),
            overwrite=True)
     
-    # reklasifikace (1,2,3)
-    grass.message("Reklasifikuji...")
+    # 2. reklasifikace (1,2,3)
+    msgr.message("Reklasifikuji...")
     # r.reclass umi reklasifikovat pouze celociselne rastry, proto pouzime
     # r.recode
-    args = {}
-    if options['classes']:
-        args['rules'] = options['classes']
-    else:
+    if not options['classes']:
         recode = """
--1:0.05:1 
-0.05:0.35:2 
-0.35:1:3
+        -1:0.05:1 
+        0.05:0.35:2 
+        0.35:1:3
         """
-        args['rules'] = '-'
-        args['stdin_'] = recode
+        kwargs = {
+            'rules' : '-',
+            'stdin_' : recode
+        }
+    else:
+        kwargs = {
+            'rules' : options['classes']
+        }
     Module('r.recode', input=ndvi, output=r_ndvi,
-           overwrite=True, **args)
+           overwrite=True, **kwargs)
     
-    # popisky
-    labels = """
-1:bez vegetace, vodni plochy
-2:plochy s minimalni vegetaci
-3:plochy pokryte vegetaci
-"""
-    Module('r.category', map=r_ndvi,
-           separator=':', rules='-', stdin_=labels)
+    # 3. tabulka barev
+    if options['classes']:
+        colors = """
+        1 red
+        2 yellow
+        3 0 136 26
+        """
+        Module('r.colors', map=r_ndvi,
+               rules='-', quiet=True, stdin_=colors)
     
-    # tabulka barev
-    colors = """
-1 red
-2 yellow
-3 0 136 26
-"""
-    Module('r.colors', map=r_ndvi,
-           rules='-', quiet=True, stdin_=colors)
-    
-    # vypsat vysledek
-    grass.message("Generuji report...")
+    # 4. vypsat vysledek
+    msgr.message("Generuji report...")
     report = Module('r.stats', flags='pl', input=r_ndvi, separator=':', stdout_=PIPE)
     
-    print('-' * 80)
-    for trida, label, procento in map(lambda x: x.split(':'), report.outputs.stdout.splitlines()):
-        print("Trida {0} ({1:28s}): {2:>7}".format(trida, label, procento))
-    print('-' * 80)
-
-    grass.message("Hotovo!")
-
+    msgr.message('-' * 80)
+    for line in report.outputs.stdout.splitlines():
+        trida, popisek, procento = line.split(':')
+        msgr.message("Trida {}: {}".format(trida, procento))
+    msgr.message('-' * 80)
+        
     return 0
 
 if __name__ == "__main__":
-    options, flags = grass.parser()
+    options, flags = parser()
     sys.exit(main())
