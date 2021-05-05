541c5b0da30d6efbcd60df49b2d4b8811bc7eb82
Martin Landa <landa.martin@gmail.com>
2021-05-05 21:51:20 +0200

    obce_wps script updated to pywps4

--- _static/skripty/obce_psc_wps.py
+++ _static/skripty/obce_psc_wps.py
@@ -1,78 +1,51 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
 import os
 import sys
-import logging
-import types
-from zipfile import ZipFile
-
-gisbase = os.environ['GISBASE'] = "/usr/lib/grass72"
-sys.path.append(os.path.join(os.environ["GISBASE"], "etc", "python"))
-os.environ['LD_LIBRARY_PATH'] = os.path.join(os.environ["GISBASE"], "lib")
+import tempfile
 
 from grass.pygrass.modules import Module
 from grass.pygrass.vector import VectorTopo
 
-from pywps.Process import WPSProcess
+from pywps import Process, LiteralInput, ComplexOutput, LOGGER, Format
 
-class Process(WPSProcess):
+class ObcePsc(Process):
      def __init__(self):
-          WPSProcess.__init__(self,
-                              identifier='obce_psc',
-                              version="1.0",
-                              title="Dotaz na obce podle PSČ",
-                              abstract="Testovací služba PyWPS.",
-                              grassLocation='gismentors_pywps',
-                              storeSupported=True,
-                              statusSupported=True)
-          
-          self.input_psc = self.addLiteralInput(identifier = "psc",
-                                                title = "Zájmové PSČ",
-                                                type = types.StringType)
-         
-          self.output = self.addComplexOutput(identifier = "output",
-                                              title = "Output zipped shapefile",
-                                              formats = [ {"mimeType":"application/x-zipped-shp"} ],
-                                              asReference = True)
+          inputs = [
+               LiteralInput(
+               identifier = "psc",
+                    title = "Zájmové PSČ",
+                    data_type="string"
+               )
+          ]
+          outputs = [
+               ComplexOutput(
+                    title = "Output GML file",
+                    supported_formats=[Format('application/gml+xml')]
+                    as_reference=True
+               )
+          ]
 
-     def export(self, map_name):
-          output_dir = os.path.join('/tmp', '{}_{}'.format(map_name, os.getpid()))
-          os.mkdir(output_dir)
-          
-          output_file = '{}/{}.zip'.format(output_dir, map_name)
-          
-          logging.debug("Export started")          
-          Module('v.out.ogr',
-                 input=map_name,
-                 output='{}/{}.shp'.format(output_dir, map_name),
-                 overwrite=True)
-          
-          os.chdir(output_dir)
-          with ZipFile(output_file, 'w') as shpzip:
-               shpzip.write('{}.dbf'.format(map_name))
-               shpzip.write('{}.shp'.format(map_name))
-               shpzip.write('{}.shx'.format(map_name))
-               shpzip.write('{}.prj'.format(map_name))
-               logging.debug("Export finished")
-          
-          self.output.setValue(output_file)
+          super().__init__(
+               self._handler,
+               identifier="obce_psc",
+               version="1.0",
+               title="Dotaz na obce podle PSČ",
+               abstract="Testovací služba PyWPS.",
+               inputs=inputs,
+               outputs=outputs,
+               grass_location="gismentors",
+               store_supported=True,
+               status_supported=True)
 
-     def execute(self):
-          map_name = self.run()
-          
-          self.export(map_name)
+          os.environ['GRASS_SKIP_MAPSET_OWNER_CHECK'] = '1'
+          os.environ['HOME'] = tempfile.gettempdir() # needed by G_home()
 
-     def run(self):
-          logging.debug("Computation started")
-
-          psc = self.input_psc.getValue()
-          map_name = 'obce_psc_{}'.format(psc)
-
-          obce = VectorTopo('obce', mapset='psc')
+     def obce_psc(psc):
+          obce = VectorTopo('obce')
           obce.open('r')
 
-          vystup = VectorTopo(map_name)
+          vystup = VectorTopo('obce_psc_{}'.format(psc))
           vystup.open('w', tab_cols=[('cat',       'INTEGER PRIMARY KEY'),
                                      ('nazev',     'TEXT'),
                                      ('psc',       'INTEGER')])
@@ -80,9 +53,9 @@ class Process(WPSProcess):
           obec_id = None
           obce_psc = set()
           for prvek in obce.viter('areas'):
-               if prvek.attrs is None:
-                    continue
-               if prvek.attrs['psc'] == psc:
+              if prvek.attrs is None:
+                  continue
+              if prvek.attrs['psc'] == psc:
                   if obec_id is None:
                       obec_id = prvek.id
 
@@ -93,7 +66,6 @@ class Process(WPSProcess):
           obce_psc.add(obec_id)
 
           hranice = list()
-          cat = 1
           for prvek in obce.viter('areas'):
               if prvek.id not in obce_psc:
                   continue
@@ -101,20 +73,29 @@ class Process(WPSProcess):
               for b in prvek.boundaries():
                   if b.id not in hranice:
                       hranice.append(b.id)
-                      vystup.write(b)
+                      vystup.write(b, attrs=(None, None))
 
-              vystup.write(prvek.centroid(), cat=cat, attrs=(prvek.attrs['nazev'], prvek.attrs['psc']))
-              cat += 1
+              vystup.write(prvek.centroid(), attrs=(prvek.attrs['nazev'], prvek.attrs['psc']))
 
           vystup.table.conn.commit()
 
           vystup.close()
           obce.close()
 
-          logging.debug("Computation finished")
+     def _handler(self, request, response):
+          psc = request.inputs['psc'][0].data
 
-          return map_name
+          LOGGER.debug("Computation started")
+          map_name = 'obce_psc_{}'.format(psc)
+          obce_psc(psc)
+          LOGGER.debug("Computation finished")
 
-if __name__ == "__main__":
-     process = Process()
-     process.execute()
+          LOGGER.debug("Export started")
+          os.chdir(self.workdir)
+          Module('v.out.ogr',
+                 input=map_name,
+                 format='GML',
+                 output='output.gml')
+          
+          response.outputs["output"].file = "output.gml"
+          return response
